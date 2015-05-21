4fa83f01e612decd79abe80198f3f35b3cd1cc84
Timothy Stott <stott.timothy@gmail.com>
2015-05-21 23:56:35 +0100

    Improve GELFUPDLogDevice specs

--- lib/loga/gelf_udp_log_device.rb
+++ lib/loga/gelf_udp_log_device.rb
@@ -3,12 +3,14 @@ require 'digest/md5'
 
 module Loga
   class GELFUPDLogDevice
-    # TODO: improve options and documentation
+    attr_reader :host, :port, :max_chunk_size, :compress
+
     def initialize(options = {})
       @max_chunk_size = 1420
-      @compress       = false
+      # Enables zlib message compression
+      @compress       = options.fetch(:compress, true)
       @socket         = UDPSocket.open
-      @host           = options.fetch(:host, '0.0.0.0') # '192.168.99.100'
+      @host           = options.fetch(:host, '127.0.0.1')
       @port           = options.fetch(:port, 12_201)
     end
 
@@ -17,16 +19,15 @@ module Loga
     end
 
     def write(message)
-      data      = (@compress ? Zlib::Deflate.deflate(message) : message).bytes
+      data      = (compress ? Zlib::Deflate.deflate(message) : message).bytes
       datagrams = []
 
-      # TODO: test fork
-      if data.count > @max_chunk_size
+      if data.count > max_chunk_size
         id    = Digest::MD5.digest(SecureRandom.uuid)[0, 8]
-        count = (data.count.to_f / @max_chunk_size).ceil
+        count = (data.count.to_f / max_chunk_size).ceil
         index = 0
 
-        data.each_slice(@max_chunk_size) do |slice|
+        data.each_slice(max_chunk_size) do |slice|
           datagrams << "\x1e\x0f" + id + [index, count, *slice].pack('C*')
           index += 1
         end
@@ -35,8 +36,12 @@ module Loga
       end
 
       datagrams.each do |datagram|
-        @socket.send(datagram, 0, @host, @port)
+        socket.send(datagram, 0, host, port)
       end
     end
+
+    private
+
+    attr_reader :socket
   end
 end
--- spec/loga/gelf_udp_log_device_spec.rb
+++ spec/loga/gelf_udp_log_device_spec.rb
@@ -3,17 +3,71 @@ require 'spec_helper'
 describe Loga::GELFUPDLogDevice do
   subject { described_class.new }
 
+  let(:host) { '127.0.0.1' }
+  let(:port) { 12_201 }
+
+  describe '#initialize' do
+    context 'defaults' do
+      specify { expect(subject.host).to eq(host) }
+      specify { expect(subject.port).to eq(port) }
+      specify { expect(subject.max_chunk_size).to eq(1420) }
+      specify { expect(subject.compress).to eq(true) }
+    end
+  end
+
   describe '#write(message)' do
-    context 'when the message in bytes is > maximu chunk size' do
-      let(:max_chunk_size) { 1420 }
-      let(:message)        { 'A' * max_chunk_size + 'B' * max_chunk_size }
+    let(:socket) { double :socket, send: nil }
 
-      subject { super().write(message) }
+    before do
+      allow(subject).to receive(:socket).and_return(socket)
+    end
 
-      pending 'sends the message in chunks'
-      it 'returns the message in chunks' do
-        expect(subject.size).to eq(2)
+    context 'when the message in bytes is < max_chunk_size' do
+      let(:message) { 'Short Message' }
+
+      context 'and compression is disabled' do
+        subject { described_class.new(compress: false) }
+
+        it 'sends the message uncompressed' do
+          expect(socket).to receive(:send).with(message, 0, host, port).once
+          subject.write(message)
+        end
       end
+
+      context 'and compression is enabled' do
+        let(:message_compressed) { Zlib::Deflate.deflate(message) }
+
+        it 'sends the message compressed' do
+          expect(socket).to receive(:send).with(message_compressed, 0, host, port).once
+          subject.write(message)
+        end
+      end
+    end
+
+    context 'when the message in bytes is > maximu chunk size' do
+      let(:message_part_1) { 'A' * subject.max_chunk_size }
+      let(:message_part_2) { 'B' * subject.max_chunk_size }
+      let(:message)        { message_part_1 + message_part_2 }
+
+      context 'and compression is disabled' do
+        subject { described_class.new(compress: false) }
+
+        it 'sends the message in uncompressed chunks' do
+          expect(socket)
+            .to receive(:send)
+            .with(/#{message_part_1}/, 0, host, port)
+            .ordered
+
+          expect(socket)
+            .to receive(:send)
+            .with(/#{message_part_2}/, 0, host, port)
+            .ordered
+
+          subject.write(message)
+        end
+      end
+
+      pending 'and compression is enabled'
     end
   end
 end
