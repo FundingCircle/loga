2d6528799762d1a222d27c0da4e2fee13ca978d0
Timothy Stott <stott.timothy@gmail.com>
2015-07-10 12:44:30 +0100

    Remove unused Utilities#unix_time_with_ms

--- lib/loga/utilities.rb
+++ lib/loga/utilities.rb
@@ -1,9 +1,5 @@
 module Loga
   module Utilities
-    def unix_time_with_ms(time)
-      "#{time.to_i}.#{time.strftime('%L')}"
-    end
-
     def duration_in_ms(started_at, ended_at)
       ((ended_at - started_at) * 1000).round
     end
