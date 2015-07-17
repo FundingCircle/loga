d52fdf9639fec899ed22a2fec21e80c706ba88bd
Dimitar Haralanov <dharalanov@me.com>
2015-07-17 16:13:46 +0300

    Remove logstash-logger gem

--- loga.gemspec
+++ loga.gemspec
@@ -19,7 +19,6 @@ Gem::Specification.new do |spec|
   spec.require_paths = ['lib']
 
   spec.add_dependency 'rack'
-  spec.add_dependency 'logstash-logger', '~> 0.10.0'
   spec.add_dependency 'activesupport', '>= 2.3.8'
 
   spec.add_development_dependency 'bundler', '~> 1.6'
--- spec/support/loga_initialize_shared.rb
+++ spec/support/loga_initialize_shared.rb
@@ -5,7 +5,7 @@ shared_context 'loga initialize' do
     Loga.configure do |config|
       config.service_name    = 'hello_world_app'
       config.service_version = '1.0'
-      config.devices         = { type: :io, io: io }
+      config.device          = io
     end
     Loga.initialize!
   end
