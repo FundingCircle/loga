840600dd9a308bcb360dd87c1a70fd49d9d5e103
Timothy Stott <stott.timothy@gmail.com>
2015-07-28 14:24:49 +0100

    Provide option to disable Loga in Rails applications

--- lib/loga/railtie.rb
+++ lib/loga/railtie.rb
@@ -5,29 +5,33 @@ module Loga
     config.loga = Loga::Configuration.new
 
     initializer :loga_initialize_logger, before: :initialize_logger do |app|
-      if Rails::VERSION::MAJOR > 3
-        config.loga.device.sync = app.config.autoflush_log
-      else
-        config.loga.device.sync = true
+      if config.loga.enable
+        if Rails::VERSION::MAJOR > 3
+          config.loga.device.sync = app.config.autoflush_log
+        else
+          config.loga.device.sync = true
+        end
+
+        config.loga.level = Logger.const_get(app.config.log_level.to_s.upcase)
+        config.loga.initialize!
+
+        app.config.logger = config.loga.logger
       end
-
-      config.loga.level = Logger.const_get(app.config.log_level.to_s.upcase)
-      config.loga.initialize!
-
-      app.config.logger = config.loga.logger
     end
 
     initializer :loga_middleware do |app|
-      app.middleware.insert_after Rails::Rack::Logger,
-                                  Loga::Rack::Logger,
-                                  app.config.logger,
-                                  app.config.log_tags,
-                                  ActionDispatch::Request
+      if config.loga.enable
+        app.middleware.insert_after Rails::Rack::Logger,
+                                    Loga::Rack::Logger,
+                                    app.config.logger,
+                                    app.config.log_tags,
+                                    ActionDispatch::Request
 
-      if config.loga.silence_rails_rack_logger
-        case Rails::VERSION::MAJOR
-        when 3 then require 'loga/ext/rails/rack/logger3.rb'
-        when 4 then require 'loga/ext/rails/rack/logger4.rb'
+        if config.loga.silence_rails_rack_logger
+          case Rails::VERSION::MAJOR
+          when 3 then require 'loga/ext/rails/rack/logger3.rb'
+          when 4 then require 'loga/ext/rails/rack/logger4.rb'
+          end
         end
       end
     end
