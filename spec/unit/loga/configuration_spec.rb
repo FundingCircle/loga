a4d44fd6883be529b8b1b963ae1c162e691f6f46
Dimitar Haralanov <dharalanov@me.com>
2016-01-11 18:24:07 +0200

    Remove duplicated specs

--- spec/unit/loga/configuration_spec.rb
+++ spec/unit/loga/configuration_spec.rb
@@ -1,38 +1,6 @@
 require 'spec_helper'
 
 describe Loga::Configuration do
-  describe Loga::RevisionStrategy do
-    describe '.call' do
-      context 'called with :git' do
-        it 'fetches the service version from git' do
-          expect(Loga::RevisionStrategy.call(:git)).to match(/\h+/)
-        end
-
-        it 'reads the REVISION file when no git repo' do
-          allow(Loga::RevisionStrategy).to receive(:fetch_from_git) { false }
-          expect(File).to receive(:read).with('REVISION').and_return('sha1_hash')
-          expect(Loga::RevisionStrategy.call(:git)).to eq('sha1_hash')
-        end
-
-        it "returns 'unknown.sha' otherwise" do
-          allow(Loga::RevisionStrategy).to receive(:fetch_from_git) { false }
-          allow(Loga::RevisionStrategy).to receive(:read_from_file) { false }
-          expect(Loga::RevisionStrategy.call(:git)).to eq('unknown.sha')
-        end
-      end
-
-      context 'called with anything else' do
-        it 'returns the argument called with' do
-          expect(Loga::RevisionStrategy.call('foobar')).to eq('foobar')
-        end
-
-        it 'strips any leading and trailing whitespace' do
-          expect(Loga::RevisionStrategy.call("\t foobar\r\n ")).to eq('foobar')
-        end
-      end
-    end
-  end
-
   subject do
     described_class.new.tap { |config| config.device = STDOUT }
   end
