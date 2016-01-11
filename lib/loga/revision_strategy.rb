694c6d3e81d40aaf4ec7154ee0d12cab9cc7a259
Dimitar Haralanov <dharalanov@me.com>
2016-01-11 18:18:09 +0200

    Make rubocop happy

--- lib/loga/revision_strategy.rb
+++ lib/loga/revision_strategy.rb
@@ -13,7 +13,7 @@ module Loga
 
       def fetch_from_git
         sha1_hash = `git rev-parse --short HEAD`
-        $?.exitstatus == 0 ? sha1_hash.strip : false
+        $CHILD_STATUS.exitstatus == 0 ? sha1_hash.strip : false
       end
 
       def read_from_file
