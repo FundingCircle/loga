c5df8571dc79111ed67d3b78e9e10c43cc3a8c3e
Timothy Stott <stott.timothy@gmail.com>
2016-10-06 17:38:29 +0100

    Remove ability to provide strategy via in service_name option
    Leave service name as nil to use strategies

---
+++ spec/unit/loga/service_version_strategies_spec.rb
@@ -0,0 +1,37 @@
+require 'spec_helper'
+
+RSpec.describe Loga::ServiceVersionStrategies do
+  describe '#call' do
+    context 'when GIT is available' do
+      before do
+        allow(described_class::SCM_GIT).to receive(:call).and_return("2776b9c\n")
+      end
+
+      it 'returns the git sha' do
+        expect(subject.call).to eql('2776b9c')
+      end
+    end
+
+    context 'when REVISION file is available' do
+      before do
+        allow(described_class::SCM_GIT).to receive(:call).and_return(nil)
+        allow(File).to receive(:read).with('REVISION').and_return("2776b9c\n")
+      end
+
+      it 'returns the file content' do
+        expect(subject.call).to eql('2776b9c')
+      end
+    end
+
+    context 'when both GIT and REVISION file are unavailable' do
+      before do
+        allow(described_class::SCM_GIT).to       receive(:call).and_return(nil)
+        allow(described_class::REVISION_FILE).to receive(:call).and_return(nil)
+      end
+
+      it 'returns a default value' do
+        expect(subject.call).to eql('unknown.sha')
+      end
+    end
+  end
+end
