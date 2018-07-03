bd7f8dfc3d5c437defdb70fef0b86159e3d20878
George Kostov <george.kostov@fundingcircle.com>
2018-07-03 16:05:05 +0300

    Refactor code in Loga::Configuration

--- lib/loga/configuration.rb
+++ lib/loga/configuration.rb
@@ -14,44 +14,42 @@ module Loga
       Sinatra::NotFound
     ].freeze
 
-    attr_accessor :device, :filter_exceptions, :filter_parameters,
-                  :host, :level, :service_version, :sync, :tags, :hide_pii
-    attr_reader :logger, :format, :service_name
+    attr_reader :device, :filter_exceptions, :filter_parameters, :format, :hide_pii,
+                :host, :level, :service_name, :service_version, :sync, :tags
 
-    # rubocop:disable Metrics/MethodLength
     def initialize(user_options = {}, framework_options = {})
       options = default_options.merge(framework_options)
                                .merge(environment_options)
                                .merge(user_options)
 
-      self.device            = options[:device]
-      self.filter_exceptions = options[:filter_exceptions]
-      self.filter_parameters = options[:filter_parameters]
-      self.format            = options[:format]
-      self.host              = options[:host]
-      self.level             = options[:level]
-      self.service_name      = options[:service_name]
-      self.service_version   = options[:service_version] || ServiceVersionStrategies.call
-      self.sync              = options[:sync]
-      self.tags              = options[:tags]
-      self.hide_pii          = options[:hide_pii]
+      @device            = options[:device]
+      @filter_exceptions = options[:filter_exceptions]
+      @filter_parameters = options[:filter_parameters]
+      @format            = options[:format].to_s.to_sym
+      @hide_pii          = options[:hide_pii]
+      @host              = options[:host]
+      @level             = options[:level]
+      @service_name      = options[:service_name].to_s.strip
+      @service_version   = options[:service_version] || ServiceVersionStrategies.call
+      @sync              = options[:sync]
+      @tags              = options[:tags]
 
       validate
-
-      @logger = initialize_logger
-    end
-    # rubocop:enable Metrics/MethodLength
-
-    def format=(name)
-      @format = name.to_s.to_sym
-    end
-
-    def service_name=(name)
-      @service_name = name.to_s.strip
     end
 
     def structured?
       format == :gelf
+    end
+
+    def logger
+      @logger ||= begin
+        device.sync          = sync
+        new_logger           = Logger.new(device)
+        new_logger.formatter = assign_formatter
+        new_logger.level     = constantized_log_level
+
+        TaggedLogging.new(new_logger)
+      end
     end
 
     private
@@ -79,14 +77,6 @@ module Loga
       { format: ENV['LOGA_FORMAT'].presence }.reject { |_, v| v.nil? }
     end
 
-    def initialize_logger
-      device.sync      = sync
-      logger           = Logger.new(device)
-      logger.formatter = assign_formatter
-      logger.level     = constantized_log_level
-      TaggedLogging.new(logger)
-    end
-
     def constantized_log_level
       Logger.const_get(level.to_s.upcase)
     end
@@ -98,15 +88,13 @@ module Loga
     end
 
     def assign_formatter
-      if format == :gelf
-        Formatters::GELFFormatter.new(
-          service_name:    service_name,
-          service_version: service_version,
-          host:            host,
-        )
-      else
-        Formatters::SimpleFormatter.new
-      end
+      return Formatters::SimpleFormatter.new if format != :gelf
+
+      Formatters::GELFFormatter.new(
+        service_name:    service_name,
+        service_version: service_version,
+        host:            host,
+      )
     end
   end
 end
