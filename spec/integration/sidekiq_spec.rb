3c73dc4b92695ce0b6723faa19bc149286fd1cbb
Aleksandar Ivanov <aleksandar.ivanov@fundingcircle.com>
2020-10-23 15:16:24 +0300

    🐞 Reproduce bug with sidekiq 6
    The JobLogger now has more methods and loga will cause the
    Sidekiq::Processor to crash with `Internal exception!`

--- spec/integration/sidekiq_spec.rb
+++ spec/integration/sidekiq_spec.rb
@@ -66,45 +66,60 @@ describe 'Sidekiq client logger' do
     it 'has the proper logger Sidekiq::Logging.logger' do
       expect(Sidekiq::Logging.logger).to eq Loga.logger
     end
+  end
 
-    # https://github.com/mperham/sidekiq/blob/97363210b47a4f8a1d8c1233aaa059d6643f5040/test/test_actors.rb#L57-L79
-    let(:mgr) do
-      Class.new do
-        attr_reader :latest_error, :mutex, :cond
+  # https://github.com/mperham/sidekiq/blob/97363210b47a4f8a1d8c1233aaa059d6643f5040/test/test_actors.rb#L57-L79
+  let(:mgr) do
+    Class.new do
+      attr_reader :latest_error, :mutex, :cond
 
-        def initialize
-          @mutex = ::Mutex.new
-          @cond = ::ConditionVariable.new
-        end
+      def initialize
+        @mutex = ::Mutex.new
+        @cond = ::ConditionVariable.new
+      end
 
-        def processor_died(_inst, err)
-          @latest_error = err
+      def processor_died(_inst, err)
+        @latest_error = err
 
-          @mutex.synchronize { @cond.signal }
-        end
+        @mutex.synchronize { @cond.signal }
+      end
 
-        def processor_stopped(_inst)
-          @mutex.synchronize { @cond.signal }
-        end
+      def processor_stopped(_inst)
+        @mutex.synchronize { @cond.signal }
+      end
 
-        def options
-          {
-            concurrency: 3,
-            queues: ['default'],
-            job_logger: Loga::Sidekiq::JobLogger,
-          }
-        end
+      def options
+        {
+          concurrency: 3,
+          queues: ['default'],
+          job_logger: Loga::Sidekiq::JobLogger,
+        }
       end
     end
+  end
 
-    it 'logs the job processing event' do
-      MySidekiqWorker.perform_async('Bob')
+  it 'logs the job processing event' do
+    MySidekiqWorker.perform_async('Bob')
 
-      require 'sidekiq/processor'
+    require 'sidekiq/processor'
+
+    if ENV['BUNDLE_GEMFILE'] =~ /sidekiq51/
       Sidekiq::Processor.new(mgr.new).start
-      sleep 0.5
+    else
+      Sidekiq::Processor.new(
+        mgr.new,
+        {
+          fetch: Sidekiq::BasicFetch.new({:queues => ['default']}),
+          job_logger: Loga::Sidekiq::JobLogger,
+        }
+      ).start
+    end
+    sleep 0.5
 
-      expected_attributes = {
+    json_line = read_json_log(line: 1)
+
+    aggregate_failures do
+      expect(json_line).to include({
         '_queue'=> 'default',
         '_retry'=> true,
         '_params'=> ['Bob'],
@@ -115,33 +130,27 @@ describe 'Sidekiq client logger' do
         '_tags'=> '',
         'level'=> 6,
         'version'=> '1.1',
-      }
+      })
 
-      json_line = read_json_log(line: 1)
-
-      aggregate_failures do
-        expect(json_line).to include(expected_attributes)
-
-        %w[_created_at _enqueued_at _jid _duration timestamp host].each do |key|
-          expect(json_line).to have_key(key)
-        end
-
-        expect(json_line['short_message']).to match(/MySidekiqWorker with jid:*/)
+      %w[_created_at _enqueued_at _jid _duration timestamp host].each do |key|
+        expect(json_line).to have_key(key)
       end
 
-      # This was a bug - the duration was constantly incresing based on when
-      # the logger was created. https://github.com/FundingCircle/loga/pull/117
-      #
-      # Test that after sleeping for few seconds the duration is still under 500ms
-      sleep 1
-
-      MySidekiqWorker.perform_async('Bob')
-
-      sleep 1
-
-      json_line = read_json_log(line: 2)
-
-      expect(json_line['_duration']).to be < 500
+      expect(json_line['short_message']).to match(/MySidekiqWorker with jid:*/)
     end
+
+    # This was a bug - the duration was constantly incresing based on when
+    # the logger was created. https://github.com/FundingCircle/loga/pull/117
+    #
+    # Test that after sleeping for few seconds the duration is still under 500ms
+    sleep 1
+
+    MySidekiqWorker.perform_async('Bob')
+
+    sleep 1
+
+    json_line = read_json_log(line: 2)
+
+    expect(json_line['_duration']).to be < 500
   end
 end
